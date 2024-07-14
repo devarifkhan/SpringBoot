9dcee2885ff6eb7f758521e11a94f1d0cd061ed4
devarifkhan <arifcse209@gmail.com>
2024-07-14 20:58:12 +0600

    running a simple web application

--- SimpleWebApplication/src/main/java/com/devarifkhan/simplewebapplication/EasySchoolApplication.java
+++ SimpleWebApplication/src/main/java/com/devarifkhan/simplewebapplication/EasySchoolApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SimpleWebApplication {
+public class EasySchoolApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SimpleWebApplication.class, args);
+        SpringApplication.run(EasySchoolApplication.class, args);
     }
 
 }
--- SimpleWebApplication/src/test/java/com/devarifkhan/simplewebapplication/SimpleWebApplicationTests.java
+++ SimpleWebApplication/src/test/java/com/devarifkhan/simplewebapplication/SimpleWebApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SimpleWebApplicationTests {
+class EasySchoolApplicationTests {
 
     @Test
     void contextLoads() {
