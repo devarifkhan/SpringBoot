152194980e134dd982eb1dba3c707223746eab92
devarifkhan <arifcse209@gmail.com>
2024-07-15 10:38:07 +0600

    dynamic content by thymeleaf

--- SimpleWebApplication/src/main/java/com/devarifkhan/simplewebapplication/controller/HomeController.java
+++ SimpleWebApplication/src/main/java/com/devarifkhan/simplewebapplication/controller/HomeController.java
@@ -1,13 +1,15 @@
 package com.devarifkhan.simplewebapplication.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"})
-    public String displayHomePage() {
-        return "home.html";
+    public String displayHomePage(Model model) {
+        model.addAttribute("username","arifkhan");
+        return "home"; // Changed from "home.html" to "home"
     }
 }
