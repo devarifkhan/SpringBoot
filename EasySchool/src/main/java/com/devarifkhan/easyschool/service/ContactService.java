894c9f1c48705c8e2bc3a25cafdd66d71d93a6fa
devarifkhan <arifcse209@gmail.com>
2024-07-16 20:39:52 +0600

    session scope

--- EasySchool/src/main/java/com/devarifkhan/easyschool/service/ContactService.java
+++ EasySchool/src/main/java/com/devarifkhan/easyschool/service/ContactService.java
@@ -5,6 +5,7 @@ import com.devarifkhan.easyschool.model.Contact;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 /*
 @Slf4j, is a Lombok-provided annotation that will automatically generate an SLF4J
@@ -12,7 +13,8 @@ Logger static property in the class at compilation time.
 * */
 @Slf4j
 @Service
-@RequestScope
+//@RequestScope
+@SessionScope
 public class ContactService {
 
     /**
