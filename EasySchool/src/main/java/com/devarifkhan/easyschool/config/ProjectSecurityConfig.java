1232ff8bd2469c8734dfc06b9025e29c9b4ace43
devarifkhan <arifcse209@gmail.com>
2024-07-18 12:53:21 +0600

    login logout implementation

--- EasySchool/src/main/java/com/devarifkhan/easyschool/config/ProjectSecurityConfig.java
+++ EasySchool/src/main/java/com/devarifkhan/easyschool/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.devarifkhan.easyschool.config;
 
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -14,18 +15,26 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
         http.csrf((csrf) -> csrf.disable())
-                .authorizeHttpRequests((requests) -> requests.requestMatchers( "/", "/home").authenticated()
+                .authorizeHttpRequests((requests) -> requests.requestMatchers("/dashboard").authenticated()
+                        .requestMatchers( "/", "/home").permitAll()
                         .requestMatchers("/holidays/**").permitAll()
                         .requestMatchers("/contact").permitAll()
                         .requestMatchers("/saveMsg").permitAll()
                         .requestMatchers("/courses").permitAll()
                         .requestMatchers("/about").permitAll()
+                        .requestMatchers("/login").permitAll()
                         .requestMatchers("/assets/**").permitAll())
-                .formLogin(Customizer.withDefaults())
+                .formLogin(loginConfigurer -> loginConfigurer.loginPage("/login")
+                        .defaultSuccessUrl("/dashboard").failureUrl("/login?error=true").permitAll())
+                .logout(logoutConfigurer -> logoutConfigurer.logoutSuccessUrl("/login?logout=true")
+                        .invalidateHttpSession(true).permitAll())
                 .httpBasic(Customizer.withDefaults());
+
         return http.build();
     }
+
     @Bean
     public InMemoryUserDetailsManager userDetailsService() {
 
@@ -41,6 +50,5 @@ public class ProjectSecurityConfig {
                 .build();
         return new InMemoryUserDetailsManager(user, admin);
     }
-
 
 }
